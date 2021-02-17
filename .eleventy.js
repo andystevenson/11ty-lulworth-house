d0286c76ffa1d163a986ce7034c31e6d0d2d4789
andystevenson <andystevenson@mac.com>
2021-02-17 10:22:16 +0000

    fixed CNAME error

--- .eleventy.js
+++ .eleventy.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
   config.addWatchTarget("./src/sass/");
   config.addPassthroughCopy("./src/css");
   config.addPassthroughCopy("./src/images");
-  config.addPassthroughCopy("./CNAME);
+  config.addPassthroughCopy("./CNAME");
 
   return {
     dir: {
